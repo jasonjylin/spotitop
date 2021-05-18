3e8d2060819c3757e293e0bb36940f8e89af1831
Jason Lin <lin.jason.jyl@gmail.com>
2021-05-17 23:32:53 -0400

    fixed top songs/artists

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,28 @@
 import React, { Component } from "react";
 import "./App.css";
-import SpotifyWebApi from "spotify-web-api-js";
-
-const spotifyApi = new SpotifyWebApi();
+import SpotifyWebApi from "spotify-web-api-node";
+const spotifyApi = new SpotifyWebApi({
+  clientId: "9de60cf124664566814ebed4b610ec20",
+  clientSecret: "33233674abb641dc8a3083503388e8ad",
+  redirectUri: "http://localhost:8000/callback",
+});
 
 class App extends Component {
   constructor() {
     super();
     const params = this.getHashParams();
+    console.log(params);
     const token = params.access_token;
+    const refresh = params.refresh_token;
     if (token) {
       spotifyApi.setAccessToken(token);
+      spotifyApi.setRefreshToken(refresh);
     }
     this.state = {
       loggedIn: token ? true : false,
       nowPlaying: { name: "Not Checked", albumArt: "" },
+      currentDisplay: "",
+      topSongs: [],
     };
   }
 
@@ -32,43 +40,80 @@ class App extends Component {
   }
 
   getNowPlaying() {
+    this.setState({
+      currentDisplay: "playing",
+    });
     spotifyApi.getMyCurrentPlaybackState().then((response) => {
+      console.log(response);
       this.setState({
         nowPlaying: {
-          name: response.item.name,
-          albumArt: response.item.album.images[0].url,
+          name: response.body.item.name,
+          albumArt: response.body.item.album.images[0].url,
         },
       });
     });
   }
 
+  getMyCurrentPlayingTrack() {
+    spotifyApi.getMyCurrentPlayingTrack().then((response) => {
+      console.log(response);
+    });
+  }
+
   getTopTracks() {
-    spotifyApi.getMyTopTracks("artists", function (error, response) {
-      if (error) {
-        console.log("error");
-      } else {
-        this.setState({
-          topArtists: {
-            aUrl: response.item[1].external_urls,
-            name: response.item[1].name,
-          },
-        });
-      }
+    this.setState({
+      currentDisplay: "song",
+    });
+    spotifyApi.getMyTopTracks().then((response) => {
+      console.log(response);
+      this.setState({
+        topSongs: response.body.items,
+      });
     });
   }
 
   render() {
-    return (
-      <div className="App">
-        <a href="http://localhost:8000"> Login </a>
-        <div>Now Playing: {this.state.nowPlaying.name}</div>
+    const logInButton = this.state.loggedIn ? (
+      <div></div>
+    ) : (
+      <a href="http://localhost:8000/login"> Login </a>
+    );
+
+    let currentDisplay = <div></div>;
+    if (this.state.currentDisplay === "playing") {
+      currentDisplay = (
         <div>
           <img src={this.state.nowPlaying.albumArt} style={{ height: 500 }} />
         </div>
+      );
+    } else if (this.state.currentDisplay === "song") {
+      console.log(this.state.topSongs);
+      currentDisplay = (
+        <div>
+          {Array.from(this.state.topSongs, (element, index) => (
+            <div>{index + 1 + ". " + element.name}</div>
+          ))}
+        </div>
+      );
+      console.log(currentDisplay);
+    } else if (this.state.currentDisplay === "artists") {
+    }
+
+    return (
+      <div className="App">
+        {logInButton}
+        <div>Now Playing: {this.state.nowPlaying.name}</div>
+        {currentDisplay}
         {this.state.loggedIn && (
-          <button onClick={() => this.getNowPlaying()}>
-            Check Now Playing
-          </button>
+          <div>
+            <button onClick={() => this.getNowPlaying()}>
+              Check Now Playing
+            </button>
+            <button onClick={() => this.getMyCurrentPlayingTrack()}>
+              Check Top Artists
+            </button>
+            <button onClick={() => this.getTopTracks()}>Check Top Songs</button>
+          </div>
         )}
       </div>
     );
